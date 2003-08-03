5edc76348bd32b0d06176c4278fe5f723ec80e41
Per Cederberg <per@percederberg.net>
2003-08-03 10:47:56 +0000

    Added various database manipulation methods for MySQL.

--- src/java/net/percederberg/liquidsite/db/MySQLDatabaseConnector.java
+++ src/java/net/percederberg/liquidsite/db/MySQLDatabaseConnector.java
@@ -21,6 +21,8 @@
 
 package net.percederberg.liquidsite.db;
 
+import java.util.ArrayList;
+
 /**
  * A MySQL database connector.
  *
@@ -73,5 +75,290 @@ public class MySQLDatabaseConnector extends DatabaseConnector {
         super("jdbc:mysql://" + host + "/" + database);
         setProperty("user", user);
         setProperty("password", password);
+    }
+
+    /**
+     * Checks if the database user in an administrator.
+     * 
+     * @return true if the database user is an administrator, or
+     *         false otherwise
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if user privileges couldn't be 
+     *             determined
+     */
+    public boolean isAdministrator() 
+        throws DatabaseConnectionException, DatabaseException {
+
+        DatabaseResults  res;
+        String           str;
+        
+        // Retrieve basic privileges
+        try {
+            res = execute("SHOW GRANTS FOR " + getDatabaseUser());
+            str = res.getRow(0).getString(0);
+        } catch (DatabaseDataException e) {
+            throw new DatabaseException("cannot determine privileges", e);
+        }
+
+        // Check privileges
+        if (str.indexOf("ALL PRIVILEGES") > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the database user being used. This is a string in the
+     * form "user@host", where the host name is the name of the 
+     * connecting host.
+     * 
+     * @return the database user
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the database user couldn't be 
+     *             determined
+     */
+    public String getDatabaseUser() 
+        throws DatabaseConnectionException, DatabaseException {
+
+        DatabaseResults  res;
+
+        try {
+            res = execute("SELECT USER()");
+            return res.getRow(0).getString(0);
+        } catch (DatabaseDataException e) {
+            throw new DatabaseException("cannot determine user", e);
+        }
+    }
+
+    /**
+     * Lists the visible databases.
+     * 
+     * @return a list of the database names
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the list of databases couldn't be 
+     *             retrieved
+     */
+    public ArrayList listDatabases()
+        throws DatabaseConnectionException, DatabaseException {
+
+        ArrayList        list = new ArrayList();
+        DatabaseResults  res;
+        
+        try {
+            res = execute("SHOW DATABASES");
+            for (int i = 0; i < res.getRowCount(); i++) {
+                list.add(res.getRow(i).getString(0));
+            }
+        } catch (DatabaseDataException e) {
+            throw new DatabaseException("cannot list databases", e);
+        }
+        return list;
+    }
+    
+    /**
+     * Lists the tables in a database. Note that this operation will
+     * return a database exception if the database user hasn't got
+     * privileges to list the tables in the database.
+     * 
+     * @param database       the database name
+     * 
+     * @return a list of the database table names
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the list of tables couldn't be 
+     *             retrieved
+     */
+    public ArrayList listTables(String database)
+        throws DatabaseConnectionException, DatabaseException {
+
+        ArrayList        list = new ArrayList();
+        DatabaseResults  res;
+        
+        try {
+            res = execute("SHOW TABLES IN " + database);
+            for (int i = 0; i < res.getRowCount(); i++) {
+                list.add(res.getRow(i).getString(0));
+            }
+        } catch (DatabaseDataException e) {
+            throw new DatabaseException("cannot list tables", e);
+        }
+        return list;
+    }
+    
+    /**
+     * Lists the users in a database. Note that this operation will
+     * return a database exception if the database user hasn't got
+     * administrator privileges.
+     * 
+     * @return a list of database user names
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the list of users couldn't be 
+     *             retrieved
+     */
+    public ArrayList listUsers()
+        throws DatabaseConnectionException, DatabaseException {
+
+        ArrayList        list = new ArrayList();
+        DatabaseResults  res;
+        String           str;
+        
+        try {
+            res = execute("SELECT DISTINCT user FROM mysql.user");
+            for (int i = 0; i < res.getRowCount(); i++) {
+                str = res.getRow(i).getString(0);
+                if (str != null && !str.equals("")) {
+                    list.add(str);
+                }
+            }
+        } catch (DatabaseDataException e) {
+            throw new DatabaseException("cannot list users", e);
+        }
+        return list;
+    }
+
+    /**
+     * Creates a new database. This operation requires that the 
+     * database user is an administrator, or a database exception 
+     * will be thrown.
+     * 
+     * @param database       the database name
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the database couldn't be created 
+     *             properly
+     */
+    public void createDatabase(String database) 
+        throws DatabaseConnectionException, DatabaseException {
+
+        execute("CREATE DATABASE " + database);
+    }
+
+    /**
+     * Deletes an existing database. This operation requires that the 
+     * database user has the correct permissions to the database, or 
+     * a database exception will be thrown.
+     * 
+     * @param database       the database name
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the database couldn't be deleted 
+     *             properly
+     */
+    public void deleteDatabase(String database) 
+        throws DatabaseConnectionException, DatabaseException {
+
+        execute("DROP DATABASE " + database);
+    }
+
+    /**
+     * Creates a new database user. This operation requires that the 
+     * current database user is an administrator, or a database 
+     * exception will be thrown. Also note that the new user will 
+     * only be given access privilege from the same host as the 
+     * current user.
+     * 
+     * @param user           the new database user name
+     * @param password       the new database user password
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the database user couldn't be 
+     *             created properly
+     */
+    public void createUser(String user, String password)
+        throws DatabaseConnectionException, DatabaseException {
+
+        String  host = getDatabaseUser();
+        
+        host = host.substring(host.indexOf("@"));
+        execute("GRANT USAGE ON *.* TO " + user + host +
+                " IDENTIFIED BY '" + password + "'");
+    }
+
+    /**
+     * Deletes an existing database user. This operation requires 
+     * that the current database user is an administrator, or a 
+     * database exception will be thrown.
+     * 
+     * @param user           the database user name
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the database user couldn't be 
+     *             deleted properly
+     */
+    public void deleteUser(String user)
+        throws DatabaseConnectionException, DatabaseException {
+
+        String  host = getDatabaseUser();
+        
+        host = host.substring(host.indexOf("@"));
+        execute("REVOKE USAGE ON *.* FROM " + user + host);
+        execute("DELETE FROM mysql.user WHERE user = '" + user + "'");
+        execute("FLUSH PRIVILEGES");
+    }
+
+    /**
+     * Adds normal access privileges to a database for a user. The
+     * access privileges are select, insert, update and delete. This 
+     * operation requires that the current database user is an 
+     * administrator, or a database exception will be thrown. Also 
+     * note that the user will only be given access privilege from 
+     * the same host as the current user. 
+     * 
+     * @param database       the database name
+     * @param user           the database user name
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the database privileges couldn't 
+     *             be set properly
+     */
+    public void addAccessPrivileges(String database, String user)
+        throws DatabaseConnectionException, DatabaseException {
+
+        String  host = getDatabaseUser();
+        
+        host = host.substring(host.indexOf("@"));
+        execute("GRANT SELECT,INSERT,UPDATE,DELETE ON " + database +
+                ".* TO " + user + host);
+    }
+
+    /**
+     * Removes normal access privileges to a database for a user. The
+     * access privileges are select, insert, update and delete. This 
+     * operation requires that the current database user is an 
+     * administrator, or a database exception will be thrown. Also 
+     * note that only the access privileges from the same host as the
+     * current user will be removed. 
+     * 
+     * @param database       the database name
+     * @param user           the database user name
+     * 
+     * @throws DatabaseConnectionException if a database connection 
+     *             couldn't be established
+     * @throws DatabaseException if the database privileges couldn't 
+     *             be set properly
+     */
+    public void removeAccessPrivilege(String database, String user)
+        throws DatabaseConnectionException, DatabaseException {
+
+        String  host = getDatabaseUser();
+        
+        host = host.substring(host.indexOf("@"));
+        execute("REVOKE SELECT,INSERT,UPDATE,DELETE ON " + database +
+                ".* FROM " + user + host);
     }
 }
