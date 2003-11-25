a64dde4c70935be7ef83a7685eb08d7919c968c6
Per Cederberg <per@percederberg.net>
2003-11-25 18:06:10 +0000

    Added online date sorting of content for non-admin applications. Moved all content sorting functionality to the content manager.

--- src/java/net/percederberg/liquidsite/content/ContentManager.java
+++ src/java/net/percederberg/liquidsite/content/ContentManager.java
@@ -24,6 +24,8 @@ package net.percederberg.liquidsite.content;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.Iterator;
 
 import net.percederberg.liquidsite.Application;
@@ -47,6 +49,20 @@ public class ContentManager {
      * The class logger.
      */
     private static final Log LOG = new Log(ContentManager.class);
+
+    /**
+     * The content name comparator. This is used by administration
+     * content managers.
+     */
+    private static final Comparator NAME_COMPARATOR =
+        new NameComparator();
+
+    /**
+     * The content online comparator. This is used by normal content
+     * managers.
+     */
+    private static final Comparator ONLINE_COMPARATOR =
+        new OnlineComparator();
 
     /**
      * The application context.
@@ -384,7 +400,7 @@ public class ContentManager {
 
         Content[]  children = InternalContent.findByParent(this, domain);
 
-        return postProcess(user, children);
+        return postProcess(user, children, isAdmin());
     }
 
     /**
@@ -409,7 +425,7 @@ public class ContentManager {
         Content[]  children;
 
         children = InternalContent.findByCategory(this, domain, category);
-        return postProcess(user, children);
+        return postProcess(user, children, isAdmin());
     }
 
     /**
@@ -429,7 +445,7 @@ public class ContentManager {
 
         Content[]  children = InternalContent.findByParent(this, parent);
 
-        return postProcess(user, children);                    
+        return postProcess(user, children, isAdmin());                    
     }
 
     /**
@@ -454,7 +470,7 @@ public class ContentManager {
         Content[]  children;
 
         children = InternalContent.findByCategory(this, parent, category);
-        return postProcess(user, children);                    
+        return postProcess(user, children, isAdmin());                    
     }
 
     /**
@@ -474,7 +490,7 @@ public class ContentManager {
 
         Content[]  children = InternalContent.findByParents(this, parents);
 
-        return postProcess(user, children);                    
+        return postProcess(user, children, false);                    
     }
 
     /**
@@ -617,13 +633,16 @@ public class ContentManager {
      *
      * @param user           the user requesting the object
      * @param content        the content objects found
+     * @param compareByName  the name-based comparison flag
      *
      * @return the readable and sorted content objects
      *
      * @throws ContentException if the database couldn't be accessed 
      *             properly
      */
-    private Content[] postProcess(User user, Content[] content)
+    private Content[] postProcess(User user,
+                                  Content[] content,
+                                  boolean compareByName)
         throws ContentException {
 
         ArrayList  list = new ArrayList(content.length);
@@ -633,10 +652,128 @@ public class ContentManager {
             if (isOnline(content[i]) && isReadable(user, content[i])) {
                 list.add(content[i]);
             }
-        } 
-        Collections.sort(list);
+        }
+        if (compareByName) {
+            Collections.sort(list, NAME_COMPARATOR);
+        } else {
+            Collections.sort(list, ONLINE_COMPARATOR);
+        }
         res = new Content[list.size()];
         list.toArray(res);
         return res;                    
+    }
+    
+    
+    /**
+     * A content name comparator. This class compares the content 
+     * categories and names to put objects in lexical order.
+     *
+     * @author   Per Cederberg, <per at percederberg dot net>
+     * @version  1.0
+     */
+    private static class NameComparator implements Comparator {
+
+        /**
+         * Compares two objects for order. Returns a negative 
+         * integer, zero, or a positive integer as the first object
+         * is less than, equal to, or greater than the second object.
+         * 
+         * @param obj1            the first object
+         * @param obj2            the second object
+         * 
+         * @return a negative integer, zero, or a positive integer as 
+         *         the first object is less than, equal to, or
+         *         greater than the second object
+         * 
+         * @throws ClassCastException if both objects aren't Content
+         *             objects
+         */
+        public int compare(Object obj1, Object obj2)
+            throws ClassCastException {
+
+            return compareInternal((Content) obj1, (Content) obj2);
+        }
+
+        /**
+         * Compares two content objects for order. Returns a negative 
+         * integer, zero, or a positive integer as the first object
+         * is less than, equal to, or greater than the second object.
+         * 
+         * @param obj1            the first content object
+         * @param obj2            the second content object
+         * 
+         * @return a negative integer, zero, or a positive integer as 
+         *         the first object is less than, equal to, or
+         *         greater than the second object
+         */
+        private int compareInternal(Content obj1, Content obj2) {
+            int category = obj1.getCategory() - obj2.getCategory();
+        
+            if (category != 0) {
+                return category;
+            } else {
+                return obj1.getName().compareTo(obj2.getName());
+            }
+        }
+    }
+
+
+    /**
+     * A content online comparator. This class compares the online
+     * publishing dates. Newer dates are considered to have 
+     * precedence over older dates.
+     *
+     * @author   Per Cederberg, <per at percederberg dot net>
+     * @version  1.0
+     */
+    private static class OnlineComparator implements Comparator {
+
+        /**
+         * Compares two objects for order. Returns a negative 
+         * integer, zero, or a positive integer as the first object
+         * is less than, equal to, or greater than the second object.
+         * 
+         * @param obj1            the first object
+         * @param obj2            the second object
+         * 
+         * @return a negative integer, zero, or a positive integer as 
+         *         the first object is less than, equal to, or
+         *         greater than the second object
+         * 
+         * @throws ClassCastException if both objects aren't Content
+         *             objects
+         */
+        public int compare(Object obj1, Object obj2)
+            throws ClassCastException {
+
+            return compareInternal((Content) obj1, (Content) obj2);
+        }
+
+        /**
+         * Compares two content objects for order. Returns a negative 
+         * integer, zero, or a positive integer as the first object
+         * is less than, equal to, or greater than the second object.
+         * 
+         * @param obj1            the first content object
+         * @param obj2            the second content object
+         * 
+         * @return a negative integer, zero, or a positive integer as 
+         *         the first object is less than, equal to, or
+         *         greater than the second object
+         */
+        private int compareInternal(Content obj1, Content obj2) {
+            Date  date1 = obj1.getOnlineDate();
+            Date  date2 = obj2.getOnlineDate();
+
+            if (date1 == null && date2 == null) {
+                return obj2.getId() - obj1.getId();
+            } else if (date1 == null) {
+                return -1;
+            } else if (date2 == null) {
+                return 1;
+            } else {
+                return date2.compareTo(date1);
+            }
+        }
     }
 }
